3a4ae1a00c19b2b3cb61f539b31f7a24d8f92423
onurkybsi <onurbpm@outlook.com>
2020-05-31 21:48:05 +0300

    /person/id endpoint was completed

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const dataStorage = require("./dataStorage");
 
-let data = dataStorage();
+let data = [];
+data = dataStorage();
 
 requestBodyExamine = (req, res, next, requiredKeys) => {
   let keys = Object.keys(req.body);
@@ -23,7 +24,7 @@ requestBodyExamine = (req, res, next, requiredKeys) => {
 const router = express.Router();
 router.use(bodyParser());
 
-router.route("").get(function (req, res, next) {
+router.route("").get(function(req, res, next) {
   res.send({
     author: "Onur Kayabasi",
     message: "Hello World!",
@@ -32,21 +33,30 @@ router.route("").get(function (req, res, next) {
 
 router
   .route("/person")
-  .get(function (req, res, next) {
+  .get(function(req, res, next) {
     res.send(data);
   })
-  .post((req, res, next) => requestBodyExamine(req, res, next, ["firstName", "lastName", "birthDate", "email"] ))
-  .post(function (req, res, next) {
-    res.send(req.body);
+  .post((req, res, next) => requestBodyExamine(req, res, next, ["firstName", "lastName", "birthDate", "email"]))
+  .post(function(req, res, next) {
+    let newItem = req.body;
+    newItem.id = data.length + 1;
+    data.push(newItem);
+    res.send(newItem);
   })
-  .all(function (req, res, next) {
+  .all(function(req, res, next) {
     res.status(404).send("Sorry can't find that!");
   });
 
-router.route;
+router.route("/person/:id").get(function(req, res, next) {
+  let id = req.params["id"] - 1;
 
-router.route("/person/:id").get(function (req, res, next) {
-  res.send(data[req.params["id"]]);
+  if (data.filter(person => person["id"] === id + 1).length === 1) {
+    res.send(data[id]);
+  } else {
+    res.status(404).send("Sorry, the requested object was not found!");
+  }
+}).all(function(req, res, next) {
+  res.status(404).send("Unavailable request!");
 });
 
 const app = express().use("/api", router).listen(3000);
